f2e381919e29915631217831607b9e919c6dcb06
leedo <lee@laylward.com>
2011-04-11 14:30:43 -0500

    break HTTPD role into Twiggy and Feersum roles

---
+++ lib/Alice/Role/HTTPD/Twiggy.pm
@@ -0,0 +1,99 @@
+package Alice::Role::HTTPD::Twiggy;
+
+use AnyEvent;
+
+use Twiggy::Server;
+use Plack::Builder;
+use Plack::Middleware::Static;
+use Plack::Session::Store::File;
+use Plack::Session::State::Cookie;
+
+use Alice::HTTP::Middleware::WebSocket;
+use Alice::HTTP::Request;
+use Alice::Stream;
+
+use Encode;
+use Try::Tiny;
+use Any::Moose 'Role';
+
+has httpd => (
+  is  => 'rw',
+  lazy => 1,
+  builder => "_build_httpd",
+);
+
+sub _build_httpd {
+  my $self = shift;
+  my $httpd;
+
+  # eval in case server can't bind port
+  try {
+    $httpd = Twiggy::Server->new(
+      host => $self->http_address,
+      port => $self->http_port,
+    );
+    $httpd->register_service(
+      builder {
+        if ($self->auth_enabled) {
+          my $session = $self->configdir."/sessions";
+          mkdir $session unless -d $session;
+          enable "Session",
+            store => Plack::Session::Store::File->new(dir => $session),
+            state => Plack::Session::State::Cookie->new(expires => 60 * 60 * 24 * 7);
+        }
+        enable "Static", path => qr{^/static/}, root => $self->assetdir;
+        enable "+Alice::HTTP::Middleware::WebSocket";
+        sub {
+          my $env = shift;
+          return sub {
+            $self->dispatch($env, shift);
+          }
+        }
+      }
+    );
+  }
+  catch {
+    warn "Error: could not start http server\n";
+    warn "$_" if $_;
+    $self->shutdown;
+  };
+
+  return $httpd;
+}
+
+sub dispatch {
+  my ($self, $env, $cb) = @_;
+
+  my $req = Alice::HTTP::Request->new($env, $cb);
+  my $res = $req->new_response(200);
+
+  if ($self->auth_enabled) {
+    unless ($req->path eq "/login" or $self->is_logged_in($req)) {
+      $self->auth_failed($req, $res);
+      return;
+    }
+  }
+
+  return $self->http_request($req, $res);
+}
+
+sub auth_failed {
+  my ($self, $req, $res) = @_;
+
+  if ($req->path =~ m{^(/(?:safe)?)$}) {
+    $res->redirect("/login".($1 ? "?dest=$1" : ""));
+    $res->body("bai");
+  } else {
+    $res->status(401);
+    $res->body("unauthorized");
+  }
+  $res->send;
+}
+
+sub is_logged_in {
+  my ($self, $req) = @_;
+  my $session = $req->env->{"psgix.session"};
+  return $session->{is_logged_in};
+}
+
+1;
