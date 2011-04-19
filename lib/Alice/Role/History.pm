5ccc9845a4ae36026d5936d6a6d16a815c10b007
leedo <lee@laylward.com>
2011-04-19 17:05:59 -0500

    irssi style logger can cycle days

--- lib/Alice/Role/History.pm
+++ lib/Alice/Role/History.pm
@@ -5,39 +5,18 @@ use AnyEvent::AIO;
 use IO::AIO;
 use File::Path qw/make_path/;
 
-requires 'configdir';
-
-has buffers => (
-  is => 'rw',
-  default => sub {{}},
-);
-
-has fhs => (
-  is => 'rw',
-  default => sub {{}},
-);
-
-has timers => (
-  is => 'rw',
-  default => sub {{}},
-);
-
 sub remember_message {
   my ($self, $window, $nick, $body) = @_;
   
-  my ($dir, $file) = $self->logfile($window);
-  my $line = $self->timestamp." <$nick> $body";
-
-  $self->_add_line($dir, $file, $line);
+  my $line = "<$nick> $body";
+  $self->add_line($window, $line);
 }
 
 sub remember_event {
   my ($self, $window, $body) = @_;
 
-  my ($dir, $file) = $self->logfile($window);
-  my $line = $self->timestamp." -!- $body";
-
-  $self->_add_line($dir, $file, $line);
+  my $line = "-!- $body";
+  $self->add_line($window, $line);
 }
 
 sub timestamp {
@@ -45,48 +24,68 @@ sub timestamp {
   my $time = sprintf("%02d:%02d", $hour, $min);
 }
 
-sub logfile {
+sub get_logfile {
   my ($self, $window) = @_;
 
-  my ($sec, $min, $hour, $day, $mon, $year) = localtime(time);
+  my @date = localtime(time);
+
+  if (!$self->{_last_day} or $self->{_last_day} != $date[3]) {
+    $self->rotate_log($window, @date);
+  }
+
+  return $self->{_last_file};
+}
+
+sub rotate_log {
+  my ($self, $window, @date) = @_;
+
+  $self->close_log;
+
+  my ($sec, $min, $hour, $day, $mon, $year) = @date;
   $year += 1900;
 
   my $dir = $self->logdir."/".$window->network."/".$window->title."/$year/";
-  my $file = sprintf("%s-%04d-%02d-%02d.txt", $window->title, $year, $mon, $day);
-
-  return ($dir, $file);
-}
-
-sub _add_line {
-  my ($self, $dir, $file, $line) = @_;
-
-  $file = "$dir/$file";
-
-  $self->buffers->{$file} ||= [];
-  my $buffer = $self->buffers->{$file};
-  push @{$buffer}, $line;
+  my $file = sprintf("%s/%s-%04d-%02d-%02d.txt", $dir, $window->title, $year, $mon, $day);
 
   make_path($dir) unless -e $dir;
 
-  if (!$self->timers->{$file}) {
-    $self->timers->{$file} = AE::timer 0, 5, sub {$self->_write_buffer($file)};
+  $self->{_last_day} = $day;
+  $self->{_last_file} = {
+    lines => ["-- Log opened ".localtime()."\n"],
+    fh => undef,
+    timer => undef,
+    path => $file
+  };
+}
+
+sub add_line {
+  my ($self, $window, $line) = @_;
+
+  my $file = $self->get_logfile($window);
+  push @{$file->{lines}}, $self->timestamp." $line\n";
+
+  if (!$file->{timer}) {
+    $file->{timer} = AE::timer 0, 5, sub {$self->_write_buffer($file)};
   }
 }
 
 sub _write_buffer {
   my ($self, $file) = @_;
-  my $output = delete $self->buffers->{$file};
 
-  if (my $fh = $self->fhs->{$file}) {
-    $self->_write($fh, $output);
+  my $lines = delete $file->{lines};
+  $file->{lines} = [];
+
+  if (my $fh = $file->{fh}) {
+    $self->_write($fh, $lines);
   }
   else {
-    aio_open $file, POSIX::O_CREAT | POSIX::O_WRONLY | POSIX::O_APPEND, 0644, sub {
+    $self->cv->begin;
+    aio_open $file->{path}, POSIX::O_CREAT | POSIX::O_WRONLY | POSIX::O_APPEND, 0644, sub {
       my $fh = shift;
       if ($fh) {
-        $self->fhs->{$file} = $fh;
-        unshift @$output, "-- Log opened ".localtime;
-        $self->_write($fh, $output);
+        $file->{fh} = $fh;
+        $self->_write($fh, $lines);
+        $self->cv->end;
       }
       else {
         warn "$!\n";
@@ -96,24 +95,26 @@ sub _write_buffer {
 }
 
 sub _write {
-  my ($self, $fh, $output, $cb) = @_;
+  my ($self, $fh, $lines) = @_;
 
-  $cb ||= sub {};
+  my $chunk = join "", @$lines;
 
-  for my $line (@$output) {
-    $line .= "\n";
-    aio_write $fh, undef, length $line, $line, 0, $cb;
+  $self->cv->begin;
+  aio_write $fh, undef, length $chunk, $chunk, 0, sub {$self->cv->end};
+}
+
+sub close_log {
+  my ($self, $file) = @_;
+  if (my $file = $self->{_last_file}) {
+    delete $file->{timer};
+    unshift @{$file->{lines}}, "-- Log closed ".localtime()."\n"; 
+    $self->_write($file->{fh}, $file->{lines});
   }
 }
 
 before shutdown => sub {
   my $self = shift;
-
-  for my $file (keys %{$self->fhs}) {
-    my $fh = delete $self->fhs->{$file};
-    $self->cv->begin;
-    $self->_write($fh, ["-- Log closed ".localtime], sub {$self->cv->end});
-  }
+  $self->close_log;
 };
 
 1;
